671e64cb9352d384ca6619a9f31d4fbbf132b27e
Sem van Nieuwenhuizen <leukenaamofzo@gmail.com>
2023-03-03 14:59:38 +0100

    FEAT: Add support for storyblok

--- sites/static/astro.config.mjs
+++ sites/static/astro.config.mjs
@@ -5,7 +5,13 @@ import compress from 'astro-compress'
 import critters from 'astro-critters'
 import image from '@astrojs/image'
 import sitemap from '@astrojs/sitemap'
+import storyblok from '@storyblok/astro'
 import tailwind from '@astrojs/tailwind'
+
+const {
+  STORYBLOK_ACCESS_TOKEN,
+  STORYBLOK_REGION,
+} = import.meta.env
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +22,13 @@ export default defineConfig({
       serviceEntryPoint: '@astrojs/image/sharp',
     }),
     sitemap(),
+    storyblok({
+      accessToken: STORYBLOK_ACCESS_TOKEN,
+      components: {},
+      apiOptions: {
+        region: STORYBLOK_REGION ?? 'eu'
+      },
+    }),
     tailwind(),
 
     // Apply the `compress` integration last for best optimizations
